0ecac987fb92eef4bc135c3571805ab3a380eec4
Viktoria <vikanikitapetrovi@gmail.com>
2026-03-06 01:17:41 +0300

    Create characters.js

---
+++ public/scr/data/characters.js
@@ -0,0 +1,26 @@
+export const characters = [
+  {
+    id: 'dog',
+    name: 'Пёсик',
+    image: '/images/characters/dog.png',
+    background: '/images/backgrounds/forest.png',
+    audio: '/audio/dog_forest_01.mp3',
+    position: 'left',
+  },
+  {
+    id: 'girl',
+    name: 'Девочка',
+    image: '/images/characters/girl.png',
+    background: '/images/backgrounds/garden.png',
+    audio: '/audio/girl_garden_01.mp3',
+    position: 'center',
+  },
+  {
+    id: 'sorceress',
+    name: 'Колдунья',
+    image: '/images/characters/sorceress.png',
+    background: '/images/backgrounds/magic_garden.png',
+    audio: '/audio/sorceress_magicgarden_01.mp3',
+    position: 'right',
+  }
+];
